9124320634073517c048bda16d9a60e869d3cf40
Bala Subramaniyan <70096022+Bala320@users.noreply.github.com>
2021-08-09 10:28:24 +0530

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,39 +1,27 @@
 import java.util.*;
-public class insertion_sort 
+
+public class Main
 {
-	
-    public static int[] insertionSort(int arr[])
-    {
-        for (int i = 1; i < arr.length; i++) 
-        { 
-            int valueToSort = arr[i];
-            int j; 
-            for ( j = i; j > 0 && arr[j - 1] > valueToSort; j--)
-            {
-                arr[j] = arr[j - 1];
-            }
- 
-            arr[j] = valueToSort;
-            System.out.print("Iteration "+(i)+": ");
-            printArray(arr);
-        }
- 
-        return arr;
-    }
-    public static void printArray(int arr[])
-    {
-        for (int i = 0; i <arr.length; i++) {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
-
-	public static void main(String args[])
-    {
-        int  arr[]={100,20,15,30,5,75};
-        insertionSort(arr);
- 
-    }
- 
-
+	public static void main(String[] args) 
+	{
+	    int ar[] = {4,3,2,10,12,1,5,6};
+	    
+	    for(int i=1;i<ar.length;i++)
+	    {
+	        int cur=ar[i];
+	        int j=i-1;
+	        
+	        while(j>=0 && ar[j]>cur)
+	        {
+	            ar[j+1]=ar[j];
+	            j=j-1;
+	        }
+	        ar[j+1]=cur;
+	    }
+	    for(int i=0;i<ar.length;i++)
+	    {
+	        System.out.println(ar[i]);
+	    }
+	    
+	}
 }
